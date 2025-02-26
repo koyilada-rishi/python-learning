594177be9f50e70e8cf1ee65a6a04d8574b4be5c
koyilada-rishi <koyilada.rishi@gmail.com>
2025-02-26 11:28:30 +0530

    Cleanup: Pushing latest changes before cleanup

---
+++ New-to-me/random.py
@@ -0,0 +1,4 @@
+##ref:- https://vscode.dev/github/koyilada-rishi/python-learning/blob/main/python-learning/DSA/Two-Pointer_Sliding-window/gpt-15.ipynb#C6:L4-L5
+## print a list 
+test_list = list(range(1,10))
+print(test_list)
